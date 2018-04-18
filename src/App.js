6e7d36eef61c6c0d50b15685769231a6c86922c4
Johan Sylvan <gtc124@gmail.com>
2018-04-18 10:09:38 +0200

    Added head

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+
+import Head from "./Head";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <main className="App">
+        <Head />
+      </main>
     );
   }
 }
---
+++ src/Head/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Head extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Giphy maybe</h1>
+            </div>
+        );
+    }
+}
+
+export default Head;
